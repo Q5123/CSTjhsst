b633f96a897ff73fedac85e809a949ce3c48f5f2
ThatOneGuy2 <georgeacarson2@gmail.com>
2017-05-18 16:00:04 -0400

    Add files via upload

--- MakeRandomMap.java
+++ MakeRandomMap.java
@@ -2,18 +2,16 @@ import java.io.*;
 public class MakeRandomMap
 {
    private int tempx, tempy;
-   public void makeDataFile()
+   public void makeDataFile() throws Exception
    {
       System.setOut(new PrintStream(new FileOutputStream("Map1.txt")));
       int numitems = (int)(Math.random() * 5 + 25);
-      int[] arrayX = new int[numitems];
-      int[] arrayY = new int[numitems];
+      int[] array = new int[numitems*2];
       System.out.println(numitems);
-      for(int k = 0; k < numitems*2; k++)
+      for(int k = 0; k < numitems*2; k+=2)
       {
-         do{
-            int temp;
-         }while();
+         array[k] = (int)Math.random() * 1600;
+         array[k+1] = (int)Math.random() * 900;
       }
    }
 }
